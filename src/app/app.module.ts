7734b0f0d9dd07ced32afb44666285f6debc22e1
mevshnu <vishnusivan1000@gmail.com>
2022-11-24 16:06:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AddComponent } from './add/add.component';
+import { DeleteComponent } from './delete/delete.component';
+import { ViewComponent } from './view/view.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeLoginComponent,
+    AdminLoginComponent,
+    AddComponent,
+    DeleteComponent,
+    ViewComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
